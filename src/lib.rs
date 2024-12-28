0613a1e22f2dda158f4323faec4bdf33a036cddb
sitetester <sitetester2010@gmail.com>
2024-12-28 08:56:03 +0200

    Update README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// It automatically acts as the entry point for declaring and organizing modules.
+// It automatically acts as the entry point (root of module tree) for declaring and organizing modules.
 // This eliminates the need to manually declare `mod api;` in `main.rs`.
 // Instead, `lib.rs` defines all of  project's modules, which can be accessed
 // by any binary (`main.rs`) or test module in the project.
